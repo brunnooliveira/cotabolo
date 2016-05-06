80572831e75098d9860d81e07dd4596311aa4410
brunnogoncalves <brunnogoncalves@gmail.com>
2016-05-06 11:31:03 -0300

    evolução do cadastro de participantes

---
+++ public/js/controller.cadastros.pagamento.modal.js
@@ -0,0 +1,27 @@
+'use strict';
+angular.module('cotabolo')
+.controller('CadastrosPagamentoController', ['$uibModalInstance', 'SaboresService', 
+	function ($uibModalInstance, SaboresService) {
+
+  var self = this;
+  var produtos;
+  
+  self.sabores;
+
+  self.ok = function (escolha) {
+  	console.log('ok');
+  	console.log(escolha);
+    $uibModalInstance.close('ok');
+  };
+
+  self.cancel = function () {
+  	console.log('cancel');
+    $uibModalInstance.dismiss('cancel');
+  };
+
+  function init(){
+  	self.sabores = SaboresService.listar();
+  };
+
+  init();
+}]);
--- public/js/service.participantes.js
+++ public/js/service.participantes.js
@@ -4,30 +4,81 @@ angular.module('cotabolo')
 	var service = {};
 	var participantes = $firebaseArray(firebaseDataService.participantes.orderByChild('posicao'));
 
+	participantes.$loaded().then(function(){
+		popularPrevisaoPagamento(participantes);
+	})
+
 	service.listar = function(){
 		return participantes;
 	}
 
 	service.incluir = function(participante){
-		participante.posicao = getUltimaPosicao();
-		participante.previsaoPagamento = (new Date()).toJSON();
-		
+		participante.posicao = getUltimoParticipante(participantes).posicao + 1;
+		var ultimoParticipante = getUltimoParticipante(participantes);
+		ordenarParticipantesPosicaoCrescente(participantes);
+		participante.previsaoPagamento = getProximaSexta(
+			ultimoParticipante
+				? new Date(ultimoParticipante.previsaoPagamento) 
+				: new Date())
+			.toJSON();
 		participantes.$add(participante);
 	}
 
 	service.excluir = function(participante){
 		participantes.$remove(participante);
+		popularPrevisaoPagamento(participantes);
+		popularPosicao(participantes);
 	}
 
-	function getUltimaPosicao(){
+	function popularPosicao(participantes){
+		angular.forEach(participantes, function(participante, key){
+			participante.posicao = key + 1;
+			participantes.$save();
+		});
+	}
+
+	function ordenarParticipantesPosicaoCrescente(participantes){
+		if(participantes && participantes.length > 0){
+			participantes.sort(function(a, b) {
+			    return parseFloat(a.posicao) - parseFloat(b.posicao);
+			});
+		}
+	}
+
+	function getUltimoParticipante(participantes){
 		if(participantes && participantes.length > 0){
 			participantes.sort(function(a, b) {
 			    return parseFloat(b.posicao) - parseFloat(a.posicao);
 			});
-			return participantes[0].posicao + 1;
+			return participantes[0];
 		}else{
-			return 1;
+			return null;
 		}
+	}
+
+	function popularPrevisaoPagamento(participantes){
+		var data = new Date();
+		var participanteIndex = 0;
+		for (var i = 0; i <= participantes.length * 7; i++) {
+			if(data.getDay() === 5){
+				participantes[participanteIndex].previsaoPagamento = new Date(data);
+				participanteIndex++;
+				if (participanteIndex === participantes.length) {
+					break;
+				}
+			}
+			data.setDate(data.getDate() + 1);
+		}
+		return participantes;
+	}
+
+	function getProximaSexta(data){
+		for (var i = 0; i <= 7; i++) {
+			data.setDate(data.getDate() + 1);
+			if(data.getDay() === 5){
+				return data
+			}
+		};
 	}
 
 	return service;
